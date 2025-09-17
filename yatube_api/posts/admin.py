058b049cd2f4c5ff848691b18590582c0429afb3
efrosim <83520884+efrosim@users.noreply.github.com>
2025-09-17 10:15:10 +0300

    Добавил регистрацию моделей в админку для соответствия требованиям

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,1 +1,37 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Comment, Follow, Group, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    search_fields = ('text',)
+    list_filter = ('pub_date', 'group')
+    list_editable = ('group',)
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'created', 'author', 'post')
+    search_fields = ('text',)
+    list_filter = ('created',)
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    empty_value_display = '-пусто-'
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'following')
+    search_fields = ('user__username', 'following__username')
+    list_filter = ('user', 'following')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Follow, FollowAdmin)
